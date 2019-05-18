a92061448d456463e684bf92f44681c4ce49f6f2
toobaditstaken <alexabbott10@yahoo.com>
2019-05-18 09:52:19 -0500

    added in displayAll function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,3 +10,26 @@ var connection = mysql.createConnection({
     password: "flash116", 
     database: "bamazon_db"
 });
+
+function displayAll() {
+    
+    connection.query('SELECT * FROM Products', function(error, response) {
+        if (error) { 
+            console.log(error) 
+        };
+        
+        var theDisplayTable = new Table({
+            head: ['Item ID', 'Product Name', 'Category', 'Price', 'Quantity'],
+            colWidths: [10, 30, 18, 10, 14]
+        });
+        for (i = 0; i < response.length; i++) {
+            theDisplayTable.push(
+                [response[i].ItemID, response[i].ProductName, response[i].DepartmentName, response[i].Price, response[i].StockQuantity]
+            );
+        }
+        console.log(theDisplayTable.toString());
+        inquireForPurchase();
+    });
+
+
+};
