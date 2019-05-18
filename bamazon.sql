8ae7049ff08c0fe60fe07466aedccd8fa74749e8
toobaditstaken <alexabbott10@yahoo.com>
2019-05-18 10:24:17 -0500

    move code in sql file to look cleaner

--- bamazon.sql
+++ bamazon.sql
@@ -10,11 +10,10 @@ CREATE TABLE Products(
 );
 
 INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Titanfall 2','Game',8.99,200);
-
-ALTER TABLE Products DROP COLUMN Price;
-ALTER TABLE Products ADD COLUMN Price DECIMAL(10,2) NOT NULL;
-
 INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Dead Space ULT','Game',5000,10);
 INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Mountain Dew','Drink',1.98,300);
 INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Doritos Cool Ranch','Food',1.78,300);
 INSERT INTO Products(ProductName,DepartmentName,Price,StockQuantity) VALUES('Broccoli','Food',2.26,200);
+
+ALTER TABLE Products DROP COLUMN Price;
+ALTER TABLE Products ADD COLUMN Price DECIMAL(10,2) NOT NULL;
